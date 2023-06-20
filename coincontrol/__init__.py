6b78c02e2aff16e24d7de628a572c394a904f103
samuel <samuelayano7@gmail.com>
2023-06-20 13:13:18 +0100

    re: refactored codebase and made some changes to application folders

--- coincontrol/__init__.py
+++ coincontrol/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
-from app.config import config
-from app.main import main
-from app.extensions import db
+from coincontrol.config import config
+from coincontrol.auth import auth
+from coincontrol.main import main
+from coincontrol.extensions import db
 
 def create_app(config_name='development'):
     app = Flask(__name__)
+    
     # setting up configuration from the development object
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
    
-    # mainblueprint registration
+    # register blueprints here
+    app.register_blueprint(auth)
     app.register_blueprint(main)
    
     # initialize the db 
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import create_app
+from coincontrol import create_app
 
 app = create_app()
 
