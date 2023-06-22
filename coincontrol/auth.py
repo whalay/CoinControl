86145babab79788de12aeba84600ae32f74d7616
samuel <samuelayano7@gmail.com>
2023-06-22 14:37:40 +0100

    update: defined new views for the main.py section

--- coincontrol/auth.py
+++ coincontrol/auth.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
 
 auth = Blueprint("auth", __name__, template_folder='templates', static_folder='static')
+
+@auth.route('/register', methods=["POST"])
+def register():
+    pass
+
+@auth.route('/login', methods=["POST"])
+def login():
+    pass
+
+@auth.route('/logout', methods=["POST"])
+def logout():
+    pass
+
--- coincontrol/main.py
+++ coincontrol/main.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 
 main = Blueprint("main", __name__, template_folder='templates', static_folder='static')
 
-@main.route('/', methods=["POST","GET"])
+@main.route('/', methods=["GET"])
 def home():
     return render_template('home.html')
 
@@ -11,5 +11,78 @@ def home():
 def dashboard():
     pass
 
+# Expenses management route
+@main.route('/expenses', methods=["GET","POST"])
+def expenses():
+    pass
 
+@main.route('/expenses/<int:id>', methods=["GET"])
+def get_expenses():
+    pass
 
+@main.route('/expenses/<int:id>', methods=["POST"])
+def update_expenses():
+    pass
+
+@main.route('/expenses/<int:id>', methods=["POST"])
+def delete_expenses():
+    pass
+
+# ---------------------------------------------------------------------------------------
+
+# Income management route
+@main.route('/income', methods=["GET","POST"])
+def income():
+    pass
+
+@main.route('/income/<int:id>', methods=["GET"])
+def get_income():
+    pass
+
+@main.route('/income/<int:id>', methods=["POST"])
+def update_income():
+    pass
+
+@main.route('/income/<int:id>', methods=["POST"])
+def delete_income():
+    pass
+
+# ---------------------------------------------------------------------------------------
+
+# Budgets management route
+@main.route('/budgets', methods=["GET","POST"])
+def budgets():
+    pass
+
+@main.route('/budgets/<int:id>', methods=["GET"])
+def get_budgets():
+    pass
+
+@main.route('/budgets/<int:id>', methods=["POST"])
+def update_budgets():
+    pass
+
+@main.route('/budgets/<int:id>', methods=["POST"])
+def delete_budgets():
+    pass
+
+# ---------------------------------------------------------------------------------------
+
+# Report and Analytics management route
+@main.route('/reports/expenses', methods=["GET"])
+def reports_expenses():
+    pass
+
+@main.route('/reports/income', methods=["GET"])
+def reports_income():
+    pass
+
+@main.route('/reports/budgets', methods=["GET"])
+def reports_budgets():
+    pass
+# ---------------------------------------------------------------------------------------
+
+# Account Settings and security route
+@main.route('/profile', methods=["GET", "POST"])
+def profile():
+    pass
