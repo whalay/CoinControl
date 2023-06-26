6dab440dd0b06e68c98afca5243fc27443eee0ea
samuel <samuelayano7@gmail.com>
2023-06-26 17:22:21 +0100

    worked on the signin page

--- coincontrol/auth.py
+++ coincontrol/auth.py
@@ -1,16 +1,17 @@
 from flask import Blueprint
+from flask import render_template, redirect
 
 auth = Blueprint("auth", __name__, template_folder='templates', static_folder='static')
 
-@auth.route('/register', methods=["POST"])
+@auth.route('/register', methods=["GET", "POST"])
 def register():
-    return "pass"
+    return render_template("auth/register.html")
 
-@auth.route('/login', methods=["POST"])
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return "pass"
+    return render_template("auth/login.html")
 
-@auth.route('/logout', methods=["POST"])
+@auth.route('/logout', methods=["GET", "POST"])
 def logout():
-    return "pass"
+    return redirect
 
